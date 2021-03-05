3ff5aa24ae510a4e8dc0d17ff93a925c8008bf94
snowmaple5891 <0602eason@gmail.com>
2021-03-06 01:03:58 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 import discord
 from discord.ext import commands
 import json
+import random
+import os
 
 with open('setting.json','r',encoding='utf8')as jfile:
     jdata = json.load(jfile)
@@ -13,14 +15,24 @@ bot = commands.Bot(command_prefix = '!', intents=intents)
 async def on_ready():
     print("bot is online")
 
-@bot.event
-async def on_member_join(member):
-    channel = bot.get_channel(int(jdata['member-alter']))
-    await channel.send(f"{member} 粗現啦!")
+@bot.command()
+async def load(ctx, extension):
+    bot.load_extension(f'cmds.{extension}')
+    await ctx.send(f'loaded {extension} done.')
 
-@bot.event
-async def on_member_remove(member):
-    channel = bot.get_channel(int(jdata['member-alter']))
-    await channel.send(f"{member} 下去啦!")
+@bot.command()
+async def unload(ctx, extension):
+    bot.unload_extension(f'cmds.{extension}')
+    await ctx.send(f'unloaded {extension} done.')
 
-bot.run(jdata['TOKEN'])
+@bot.command()
+async def reload(ctx, extension):
+    bot.reload_extension(f'cmds.{extension}')
+    await ctx.send(f'reloaded {extension} done.')
+
+for filename in os.listdir('./cmds'):
+    if filename.endswith(".py"):
+        bot.load_extension(f'cmds.{filename[:-3]}')
+    
+if __name__ == "__main__":
+    bot.run(jdata['TOKEN'])
